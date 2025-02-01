626995012f854d3a79253da0b40387eca8844dbc
rajk2133 <singh.honey046@gmail.com>
2025-02-01 18:05:47 +0530

    fav icon

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -90,7 +90,7 @@ export default function Register() {
         <TextField
           label="Age"
           name="age"
-          type="text"
+          type="number"
           autoComplete="family-name"
           value={formData.age}
           onChange={handleChange}
