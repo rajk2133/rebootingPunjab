6534cd47e635806a52b2d8c76054a9ae603725fa
rajk2133 <singh.honey046@gmail.com>
2025-02-01 18:25:18 +0530

    api

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -8,6 +8,7 @@ import Image from 'next/image'
 import { useState } from 'react'
 import axios from 'axios'
 import backgroundImage from '@/images/mappunjab.png'
+import { toast,ToastContainer } from 'react-toastify'
 
 export default function Register() {
   const [formData, setFormData] = useState({
@@ -31,113 +32,119 @@ export default function Register() {
       const response = await axios.post('http://localhost:8080/incubation/data', formData)
       console.log(response)
       if (response.status === 200) {
-        alert('Registration successful!')
+        toast.success('Registration successful!')
+      }
+      else if (response.status == 400) {
+        toast.success('User already Registered')
       }
     } catch (error) {
       console.error('Error registering:', error)
-      alert('Failed to register. Please try again.')
+      toast.error('Something went wrong!')
     }
   }
   return (
-    <SlimLayout>
-      <div className="flex">
-        <Link href="/" aria-label="Home">
-          <Image src={logo} />
-        </Link>
-      </div>
-
-      <Image
-        className="sm:hidden rounded-xl mt-4"
-        src={backgroundImage}
-        alt=""
-        unoptimized
-      />
-
-      <form
-        onSubmit={handleSubmit}
-        className="mt-5 grid grid-cols-1 gap-x-6 gap-y-3 sm:grid-cols-2"
-      >
-        <TextField
-          label="Name"
-          name="name"
-          type="text"
-          autoComplete="given-name"
-          className="col-span-full"
-          value={formData.name}
-          onChange={handleChange}
-          required
-        />
-        <TextField
-          className="col-span-full"
-          label="Email address"
-          name="email"
-          type="email"
-          value={formData.email}
-          onChange={handleChange}
-          autoComplete="email"
-          required
-        />
-        <TextField
-          className="col-span-full"
-          label="Mobile Number"
-          name="mobile"
-          type="number"
-          autoComplete="mobile"
-          value={formData.mobile}
-          onChange={handleChange}
-          required
-        />
-        <TextField
-          label="Age"
-          name="age"
-          type="number"
-          autoComplete="family-name"
-          value={formData.age}
-          onChange={handleChange}
-          required
-        />
-
-        <SelectField
-          className="h-auto w-auto"
-          label="Gender"
-          name="gender"
-          value={formData.gender}
-          onChange={handleChange}
-        >
-          <option value="">Select gender</option>
-          <option value="Male">Male</option>
-          <option value="Female">Female</option>
-
-        </SelectField>
-        <TextField
-          className="col-span-full"
-          label="Profession"
-          name="profession"
-          type="text"
-          autoComplete="profession"
-          value={formData.profession}
-          onChange={handleChange}
-          required
-        />
-        <TextField
-          className="col-span-full"
-          label="Region"
-          name="region"
-          type="text"
-          autoComplete="region"
-          value={formData.region}
-          onChange={handleChange}
-          required
-        />
-
-        <div className="col-span-full mt-4">
-          <Button type="submit" className="w-full bg-[#da8b57]">
-            <span className='text-white'>
-              Submit <span aria-hidden="true">&rarr;</span>
-            </span>
-          </Button>
+    <>
+      <ToastContainer/>
+      <SlimLayout>
+        <div className="flex">
+          <Link href="/" aria-label="Home">
+            <Image src={logo} />
+          </Link>
         </div>
-      </form>
-    </SlimLayout>
+
+        <Image
+          className="sm:hidden rounded-xl mt-4"
+          src={backgroundImage}
+          alt=""
+          unoptimized
+        />
+
+        <form
+          onSubmit={handleSubmit}
+          className="mt-5 grid grid-cols-1 gap-x-6 gap-y-3 sm:grid-cols-2"
+        >
+          <TextField
+            label="Name"
+            name="name"
+            type="text"
+            autoComplete="given-name"
+            className="col-span-full"
+            value={formData.name}
+            onChange={handleChange}
+            required
+          />
+          <TextField
+            className="col-span-full"
+            label="Email address"
+            name="email"
+            type="email"
+            value={formData.email}
+            onChange={handleChange}
+            autoComplete="email"
+            required
+          />
+          <TextField
+            className="col-span-full"
+            label="Mobile Number"
+            name="mobile"
+            type="number"
+            autoComplete="mobile"
+            value={formData.mobile}
+            onChange={handleChange}
+            required
+          />
+          <TextField
+            label="Age"
+            name="age"
+            type="number"
+            autoComplete="family-name"
+            value={formData.age}
+            onChange={handleChange}
+            required
+          />
+
+          <SelectField
+            className="h-auto w-auto"
+            label="Gender"
+            name="gender"
+            value={formData.gender}
+            onChange={handleChange}
+          >
+            <option value="">Select gender</option>
+            <option value="Male">Male</option>
+            <option value="Female">Female</option>
+
+          </SelectField>
+          <TextField
+            className="col-span-full"
+            label="Profession"
+            name="profession"
+            type="text"
+            autoComplete="profession"
+            value={formData.profession}
+            onChange={handleChange}
+            required
+          />
+          <TextField
+            className="col-span-full"
+            label="Region"
+            name="region"
+            type="text"
+            autoComplete="region"
+            value={formData.region}
+            onChange={handleChange}
+            required
+          />
+
+          <div className="col-span-full mt-4">
+            <Button type="submit" className="w-full bg-[#da8b57]">
+              <span className='text-white'>
+                Submit <span aria-hidden="true">&rarr;</span>
+              </span>
+            </Button>
+          </div>
+        </form>
+      </SlimLayout>
+    </>
   )
 }
